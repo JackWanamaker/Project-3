fb0a0ed8f0ab6da8c9665c90fed7c442548686d8
bignoseben <jackwanamaker49@gmail.com>
2024-03-28 00:47:40 -0400

    Began fleshing out playlist. Realized I need to research Maven, and switch over to my Ubuntu install. C u there! :-)

--- main/Library.java
+++ main/Library.java
@@ -1,5 +1,16 @@
 package main;
 
-public class Library {
+public interface Library<T> {
     
+    /*
+     *  a String with all songs in the library,
+     * ordered alphabetically by Artist, then song title.
+     */
+    String alphabetical_list();
+
+
+    /*
+     * Returns a string with the information about a song
+     * in the library. 
+     */
 }
--- main/Playlist.java
+++ main/Playlist.java
@@ -1,5 +1,5 @@
 package main;
 
-public class Playlist {
+public interface Playlist {
     
 }
